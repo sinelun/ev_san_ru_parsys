37fb2d2a5e3ef686cb92fe660c65b29e349a24b2
sinelun <sinelun@gmail.com>
2020-07-30 17:58:23 +0300

    Finished parsing.admin.brand

--- parsys/parsing/admin/brand.py
+++ parsys/parsing/admin/brand.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from ..models import Brand
+from ..models import Brand, BrandSite, BrandSiteMapping
 
 
 @admin.register(Brand)
@@ -22,3 +22,36 @@ class BrandAdmin(admin.ModelAdmin):
     def has_delete_permission(self, request, obj=None):
         return False
 
+
+def map_brands(modeladmin, request, queryset):
+    """ Сопоставление брендов.
+    """
+    for obj in queryset:
+        site_brand = BrandSite.objects.filter(name__iexact=obj.brand.manufacturer.name).first()
+        print(obj.brand.manufacturer.name, site_brand)
+        obj.site_brand = site_brand
+        obj.save()
+
+
+map_brands.short_description = 'Сопоставить выделенные бренды'
+
+
+@admin.register(BrandSiteMapping)
+class BrandSiteMappingAdmin(admin.ModelAdmin):
+    list_display = ('brand_name', 'site_brand',)
+    list_select_related = ('brand', 'site_brand',)
+    list_editable = ('site_brand',)
+    search_fields = ('brand__manufacturer__name', 'site_brand__name')
+
+    def brand_name(self, obj):
+        return obj.brand.manufacturer.name
+    brand_name.short_description = 'Бренд'
+    brand_name.admin_order_field = 'brand__manufacturer__name'
+
+    actions = [map_brands]
+
+    # def has_add_permission(self, request, obj=None):
+    #     return False
+    #
+    # def has_delete_permission(self, request, obj=None):
+    #     return False
